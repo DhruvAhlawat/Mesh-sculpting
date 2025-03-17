7aa819d986eb64d37255200717e2cfa810a60886
Aditya Upadhyay <me1210275@iitd.ac.in>
2025-03-17 23:48:39 +0530

    Fixed cube

--- src/mesh.hpp
+++ src/mesh.hpp
@@ -172,5 +172,5 @@ Mesh generateCube(int m, int n, int o);
 Mesh loadOBJ(const std::string& filename);
 void getMeshFromVerts(Mesh &m, std::vector<vec3> &vertexPositions, std::vector<std::vector<int>> &faces, std::vector<vec3> normals = {});
 void umbrellaSmooth(Mesh &m, float lambda, int iterations = 1);
-HalfEdge *prev(HalfEdge *he);
-// void getMeshFromVerts(Mesh &m, vector<vec3> &vertexPositions, vector<vector<int>> &faces);
+HalfEdge *prev(HalfEdge *he);   // returns the previous half-edge in the face
+void catmullClarkSubdivision(Mesh &m);
